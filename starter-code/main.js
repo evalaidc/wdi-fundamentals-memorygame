96d095ba6a8d2c20527393dc19c3c8630b24f583
Eva Lai <evalai.dc@gmail.com>
2017-01-16 20:58:48 -0500

    JavaScript of cards and flip

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,40 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+ var cards = ["queen","queen","king","king"];
 
-if (cardTwo === cardFour) {
-	alert('You found a match!');
+ var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+function createBoard () {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+
+		board.appendChild(cardElement);
+		board.appendChild(cardElement);
+	}
 }
-else {
-	alert('Sorry, try again.')
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  	console.log(this.getAttribute('data-card'));
+  	if(this.getAttribute('data-card') === 'king') {
+  		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+  	} else {
+  		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+  	}
+  	this.getAttribute('data-card').className = 'img';
+  	if (cardsInPlay.length === 2) {
+  		isMatch(cardInPlay);
+  		cardsInPlay =[];
+  	}
+
 }
+function isMatch(cards) {
+		if (cards[0]===cards[1]) {
+			alert('You found a match!');
+		} else {
+			alert('Sorry, try again.')
+		}	
+}
+createBoard();
